9f7fdff04e2d6e7c757f715c635427366bf43686
Fatin-Athira <athirafatin93@gmail.com>
2022-07-31 13:43:24 +0800

    issuebook and return book

---
+++ IssueBook.py
@@ -0,0 +1,125 @@
+from tkinter import *
+from tkinter import messagebox
+import pymysql
+
+# Add database 
+
+mydatabase="db"
+
+con = pymysql.connect(host="localhost",user="root",password="",database=mydatabase)
+cur = con.cursor()
+
+# Enter Table Names here
+issueTable = "rental" 
+bookTable = "books"
+    
+#List To store all Book IDs
+allbid = [] 
+
+def issue():
+    
+    global issueBtn,labelFrame,lb1,inf1,inf2,quitBtn,root,Canvas1,status
+    
+    bid = inf1.get()
+    name = inf2.get()
+
+    issueBtn.destroy()
+    labelFrame.destroy()
+    lb1.destroy()
+    inf1.destroy()
+    inf2.destroy()
+    
+    
+    extractbid = "select bid from "+bookTable
+    try:
+        cur.execute(extractbid)
+        con.commit()
+        for i in cur:
+            allbid.append(i[0])
+        
+        if bid in allbid:
+            checkAvail = "select status from "+bookTable+" where bid = '"+bid+"'"
+            cur.execute(checkAvail)
+            con.commit()
+            for i in cur:
+                check = i[0]
+                
+            if check == 'avail':
+                status = True
+            else:
+                status = False
+
+        else:
+            messagebox.showinfo("Error","Book ID not present")
+    except:
+        messagebox.showinfo("Error","Can't fetch Book IDs")
+    
+    issueSql = "insert into "+issueTable+" values ('"+bid+"','"+name+"')"
+    show = "select * from "+issueTable
+    
+    updateStatus = "update "+bookTable+" set status = 'issued' where bid = '"+bid+"'"
+    try:
+        if bid in allbid and status == True:
+            cur.execute(issueSql)
+            con.commit()
+            cur.execute(updateStatus)
+            con.commit()
+            messagebox.showinfo('Success',"Book Issued Successfully")
+            root.destroy()
+        else:
+            allbid.clear()
+            messagebox.showinfo('Message',"Book Already Issued")
+            root.destroy()
+            return
+    except:
+        messagebox.showinfo("Search Error","The value entered is wrong, Try again")
+    
+    print(bid)
+    print(name)
+    
+    allbid.clear()
+    
+def issueBook(): 
+    
+    global issueBtn,labelFrame,lb1,inf1,inf2,quitBtn,root,Canvas1,status
+    
+    root = Tk()
+    root.title("Library")
+    root.minsize(width=400,height=400)
+    root.geometry("600x500")
+    
+    Canvas1 = Canvas(root)
+    Canvas1.config(bg="#770E0E")
+    Canvas1.pack(expand=True,fill=BOTH)
+
+    headingFrame1 = Frame(root,bg="#FFBB00",bd=5)
+    headingFrame1.place(relx=0.25,rely=0.1,relwidth=0.5,relheight=0.13)
+        
+    headingLabel = Label(headingFrame1, text="Issue Book", bg='black', fg='white', font=('Courier',15))
+    headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
+    
+    labelFrame = Frame(root,bg='black')
+    labelFrame.place(relx=0.1,rely=0.3,relwidth=0.8,relheight=0.5)  
+        
+    # Book ID
+    lb1 = Label(labelFrame,text="Book Id: ", bg='black', fg='white')
+    lb1.place(relx=0.05,rely=0.2)
+        
+    inf1 = Entry(labelFrame)
+    inf1.place(relx=0.3,rely=0.2, relwidth=0.62)
+    
+    # Issued To Student name 
+    lb2 = Label(labelFrame,text="Student Name : ", bg='black', fg='white')
+    lb2.place(relx=0.05,rely=0.4)
+        
+    inf2 = Entry(labelFrame)
+    inf2.place(relx=0.3,rely=0.4, relwidth=0.62)
+
+    #Issue Button
+    issueBtn = Button(root,text="Issue Book",bg='#ffffff', fg='black',bd=0,command=issue)
+    issueBtn.place(relx=0.28,rely=0.9, relwidth=0.18,relheight=0.08)
+    
+    quitBtn = Button(root,text="EXIT",bg='#ffffff', fg='black',bd=0, command=root.destroy)
+    quitBtn.place(relx=0.53,rely=0.9, relwidth=0.18,relheight=0.08)
+    
+    root.mainloop()
